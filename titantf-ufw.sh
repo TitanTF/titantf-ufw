1e2e9d405e4f5d80be5dd4ce04f4ac3a41cf59d4
myst <myst@titan.tf>
2023-02-18 17:58:41 +0800

    Update titantf-ufw.sh

--- titantf-ufw.sh
+++ titantf-ufw.sh
@@ -3,7 +3,7 @@
 curl -s https://raw.githubusercontent.com/TitanTF/titantf-ufw/master/ipv4.txt -o /tmp/titantf_ips
 echo "" >> /tmp/titantf_ips
 
-# Allow all traffic from Cloudflare IPs (no ports restriction)
+# Allow all traffic from Titan.TF IPs (no ports restriction)
 for cfip in `cat /tmp/titantf_ips`; do ufw allow proto tcp from $cfip comment 'titan.tf IP'; done
 
 ufw reload > /dev/null
